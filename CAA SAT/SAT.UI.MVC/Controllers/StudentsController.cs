ab80014adf3df6b23639b896701ff79ba294b805
Asche Jurotich <asche.jurotich@gmail.com>
2024-10-15 19:24:39 -0500

    Image uploading for edit view, and displaying picture in details view

--- CAA SAT/SAT.UI.MVC/Controllers/StudentsController.cs
+++ CAA SAT/SAT.UI.MVC/Controllers/StudentsController.cs
@@ -12,9 +12,9 @@ using System.Drawing;
 namespace SAT.UI.MVC.Controllers {
 	public class StudentsController : Controller {
 		private readonly SatContext _context;
-        private readonly IWebHostEnvironment _webHostEnvironment;
+		private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public StudentsController(SatContext context, IWebHostEnvironment iwhb) {
+		public StudentsController(SatContext context, IWebHostEnvironment iwhb) {
 			_context = context;
 			_webHostEnvironment = iwhb;
 
@@ -55,70 +55,65 @@ namespace SAT.UI.MVC.Controllers {
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> Create([Bind("StudentId,FirstName,LastName,FullName,Major,Address,City,State,ZipCode,Phone,Email,PhotoUrl,Image,Ssid")] Student student) {
 			if(ModelState.IsValid) {
-                #region File Upload - Create
-                if (student.Image != null)
-                {
-                    //Check the file type
-                    // retrieve the extension of the uploaded file
-                    string ext = Path.GetExtension(student.Image.FileName);
+				#region File Upload - Create
+				if(student.Image != null) {
+					//Check the file type
+					// retrieve the extension of the uploaded file
+					string ext = Path.GetExtension(student.Image.FileName);
 
-                    //Make a list of valid extensions to check against
-                    string[] validExts = { ".jpg", ".jpeg", ".gif", ".png" };
+					//Make a list of valid extensions to check against
+					string[] validExts = { ".jpg", ".jpeg", ".gif", ".png" };
 
-                    //check that the file has an extension in our list AND
-                    //check if the file size will work with our .NET app
-                    if (validExts.Contains(ext.ToLower()) && student.Image.Length < 4_194_303)
-                    {
-                        //Generate a unique filename
-                        student.PhotoUrl = Guid.NewGuid() + ext;
+					//check that the file has an extension in our list AND
+					//check if the file size will work with our .NET app
+					if(validExts.Contains(ext.ToLower()) && student.Image.Length < 4_194_303) {
+						//Generate a unique filename
+						student.PhotoUrl = Guid.NewGuid() + ext;
 
-                        //Save the file to the web server (here, saving to wwwroot/images)
-                        //To access wwwroot, add a property to the controller for the
-                        //_webHostEnvironment (see the top of this class for our example)
-                        //Retrieve the path to wwwroot
-                        string webRootPath = _webHostEnvironment.WebRootPath;
-                        //variable for the full image path must include the images folder:
-                        string fullImagePath = webRootPath + "/images/";
+						//Save the file to the web server (here, saving to wwwroot/images)
+						//To access wwwroot, add a property to the controller for the
+						//_webHostEnvironment (see the top of this class for our example)
+						//Retrieve the path to wwwroot
+						string webRootPath = _webHostEnvironment.WebRootPath;
+						//variable for the full image path must include the images folder:
+						string fullImagePath = webRootPath + "/img/Clown/";
 
-                        //Create a MemoryStream to read the image into the server memory
-                        using (var memoryStream = new MemoryStream())
-                        {
-                            //transfer the file from the request into our server's memory
-                            await student.Image.CopyToAsync(memoryStream);
-                            //add a using statement for the Image class using System.Drawing
-                            using (var img = Image.FromStream(memoryStream))
-                            {
-                                //now, send the image to the ImageUtility for resizing and thumbnail creation
-                                //items needed for the ImageUtility.ResizeImage()
-                                //1) (int) maximum image size
-                                //2) (int) maximum thumbnail image size
-                                //3) (string) full path where the file will be saved
-                                //4) (Image) an image
-                                //5) (string) filename
-                                int maxImageSize = 500;
-                                int maxThumbSize = 100;
+						//Create a MemoryStream to read the image into the server memory
+						using(var memoryStream = new MemoryStream()) {
+							//transfer the file from the request into our server's memory
+							await student.Image.CopyToAsync(memoryStream);
+							//add a using statement for the Image class using System.Drawing
+							using(var img = Image.FromStream(memoryStream)) {
+								//now, send the image to the ImageUtility for resizing and thumbnail creation
+								//items needed for the ImageUtility.ResizeImage()
+								//1) (int) maximum image size
+								//2) (int) maximum thumbnail image size
+								//3) (string) full path where the file will be saved
+								//4) (Image) an image
+								//5) (string) filename
+								int maxImageSize = 500;
+								int maxThumbSize = 100;
 
-                                ImageUtility.ResizeImage(fullImagePath, student.PhotoUrl, img, maxImageSize, maxThumbSize);
+								ImageUtility.ResizeImage(fullImagePath, student.PhotoUrl, img, maxImageSize, maxThumbSize);
 
-                                //if we weren't handling resizing of images, we could plainly save a file like this:
-                                //myFile.Save("path/to/save", "filename")
+								//if we weren't handling resizing of images, we could plainly save a file like this:
+								//myFile.Save("path/to/save", "filename")
 
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    //If no image was uploaded, assign a default filename
-                    //We also added noimage.png to our images folder
-                    student.PhotoUrl = "SadClown.jpg";
-                }
-                //IMAGE UPLOAD - STEP 12
-                //Add noimage.png to our images folder
+							}
+						}
+					}
+				}
+				else {
+					//If no image was uploaded, assign a default filename
+					//We also added noimage.png to our images folder
+					student.PhotoUrl = "SadClown.jpg";
+				}
+				//IMAGE UPLOAD - STEP 12
+				//Add noimage.png to our images folder
 
-                #endregion
+				#endregion
 
-                _context.Add(student);
+				_context.Add(student);
 				await _context.SaveChangesAsync();
 				return RedirectToAction(nameof(Index));
 			}
@@ -145,23 +140,72 @@ namespace SAT.UI.MVC.Controllers {
 		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("StudentId,FirstName,LastName,Major,Address,City,State,ZipCode,Phone,Email,PhotoUrl,Ssid")] Student student) {
+		public async Task<IActionResult> Edit(int id, [Bind("StudentId,FirstName,LastName,Major,Address,City,State,ZipCode,Phone,Email,PhotoUrl,Image,Ssid")] Student student) {
 			if(id != student.StudentId) {
 				return NotFound();
 			}
 
 			if(ModelState.IsValid) {
+
+				#region file upload - edit
+
+				//save old image name to delete if replacement comes
+				string oldImgName = student.PhotoUrl;
+
+				if(student.Image != null) {
+					//allowed extensions list
+					string[] validExts = { ".jpg", ".jpeg", ".png", ".gif" };
+
+					//get the extension
+					string ext = Path.GetExtension(student.Image.FileName);
+
+					//check if file has valid extensions
+					// and if size will work with .net app
+					if(validExts.Contains(ext.ToLower()) && student.Image.Length < 4_194_303) {
+
+						//make unique file name
+						student.PhotoUrl = Guid.NewGuid() + ext;
+
+						//Save the file to the web server (here, saving to wwwroot/images)
+						//To access wwwroot, add a property to the controller for the
+						//_webHostEnvironment (see the top of this class for our example)
+						//Retrieve the path to wwwroot
+						string webRootPath = _webHostEnvironment.WebRootPath;
+
+						//full image path must include images folder
+						string fullImagePath = webRootPath + "/img/Clown/";
+
+						//delete old image
+						if(oldImgName != fullImagePath + "placeholder.png")
+							ImageUtility.Delete(fullImagePath, oldImgName);
+
+						//save new image
+						using(var memoryStream = new MemoryStream()) {
+							await student.Image.CopyToAsync(memoryStream);
+
+							using(var img = Image.FromStream(memoryStream)) {
+								int maxImageSize = 500;
+								int maxThumbSize = 100;
+
+								ImageUtility.ResizeImage(fullImagePath, student.PhotoUrl, img, maxImageSize, maxThumbSize);
+							}
+
+						}
+
+					}
+
+				}
+
+				#endregion
+
 				try {
 					_context.Update(student);
 					await _context.SaveChangesAsync();
 				}
 				catch(DbUpdateConcurrencyException) {
-					if(!StudentExists(student.StudentId)) {
+					if(!StudentExists(student.StudentId)) 
 						return NotFound();
-					}
-					else {
-						throw;
-					}
+					else throw;
 				}
 				return RedirectToAction(nameof(Index));
 			}
